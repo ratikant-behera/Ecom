1e9d1f9f7dc2b9e904acbe6687d52b09ce7b9c65
ratikant-behera <beheraratikanta436@gmail.com>
2024-12-24 22:37:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const shopReviewRouter = require("./routes/shop/review-routes");
 
 const commonFeatureRouter = require("./routes/common/feature-routes");
 
-//create a database connection -> u can also
+
 //create a separate file for this and then import/use that file here
 
 // mongoose
